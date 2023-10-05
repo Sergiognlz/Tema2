6319aafd2cd7ba2653b8b68a4a2a017eec0861d5
Sergiognlz <sergio.gonzalez@iesnervion.es>
2023-10-05 12:40:41 +0200

    9

---
+++ src/tema2/Ejercicio9.java
@@ -0,0 +1,20 @@
+package tema2;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+public static void main(String[] args) {
+	//creamos  constantes para cada figura
+	boolean piedra,papel,tijera;
+	//creamos variables para cada jugador
+	String jugador1, jugador2;
+	// creamos escanner
+	Scanner sc=new Scanner(System.in);
+	
+	//pedimnos al usuario que introduzca una jugada
+	
+	
+	
+	
+}
+}
