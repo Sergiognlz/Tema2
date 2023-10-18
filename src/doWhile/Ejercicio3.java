ac694f677791dba8e08ef3f3cd75f714edf595f0
Sergiognlz <sergio.gonzalez@iesnervion.es>
2023-10-18 09:43:35 +0200

    3

---
+++ src/doWhile/Ejercicio3.java
@@ -0,0 +1,27 @@
+package doWhile;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+	public static void main(String[] args) {
+//creamos variables
+	int numero, contador=0, suma=0;
+	//creamos escaner
+	Scanner sc=new Scanner(System.in);
+	//pedimos al usuario que introduzca un número.
+	System.out.println("Introduce un número");
+	//leemos el número
+	numero=sc.nextInt();
+	//creamos el do
+	do {
+		//especificamos las variables que se ejecutarán
+		suma+=contador++;
+		//cerramos el bucle con el while
+	}while (contador<=numero);
+	System.out.println("La suma de los números hasta el "+numero+" es: " +suma);
+	//cerramos el escaner
+	sc.close();
+	
+	}
+}
+
---
+++ src/doWhile/Ejercicio4.java
@@ -0,0 +1,28 @@
+package doWhile;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+	public static void main(String[] args) {
+		// creamos variables
+		int numero, contador = 0;
+		// creamos el escaner
+		Scanner sc = new Scanner(System.in);
+
+		// pedimos al usuario un número que mostrar su tabla de multiplicar
+		System.out.println("Introduce un número para ver su tabla de multiplicar");
+		// leemos el número
+		numero = sc.nextInt();
+		// creamos bucle iniciando con do
+		do {
+			contador++;
+			// mostramos tabla de multiplicar
+			System.out.println(numero + "x" + contador + "=" + numero * contador);
+			//establecemos el valor que hará que el bucle se repita
+			//en este caso ponemos 9 porque si ponemos 10 hace 11 operaciones
+		} while (contador <= 9);
+
+		sc.close();
+
+	}
+}
