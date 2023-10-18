701a8cf1759f4a37a4e3bd2ba59b56d616e90f34
Sergio <Sergio@192.168.1.135>
2023-10-18 20:08:10 +0200

    23

---
+++ src/ejerciciosDoWhile/Ejercicio1.java
@@ -0,0 +1,16 @@
+package ejerciciosDoWhile;
+
+public class Ejercicio1 {
+	public static void main(String[] args) {
+		// creamos las variables a usar
+		int contador = 0;
+
+		// creamos bucle con do while poniendo primero las instrucciones que se van a
+		// llevar a cabo a continuación.
+		do {
+
+			System.out.println(contador);
+			contador++;
+		} while (contador <= 20);
+	}
+}
---
+++ src/ejerciciosDoWhile/Ejercicio2.java
@@ -0,0 +1,20 @@
+package ejerciciosDoWhile;
+
+public class Ejercicio2 {
+	public static void main(String[] args) {
+		// creamos las variables a usar
+		int contador = 0; 
+		
+
+		// creamos bucle con do while poniendo primero las instrucciones que se van a
+		// llevar a cabe a continuación.
+		do {
+		contador++;
+		
+			if(contador%2==0) {
+				System.out.println(contador);}
+		} while(contador <= 200);{
+	
+	}
+		}}
+	
