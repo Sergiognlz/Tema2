4cbfd4af0c69219d7e3873f93f160da696226783
Sergiognlz <sergio.gonzalez@iesnervion.es>
2023-10-20 13:59:25 +0200

    6

--- src/ejerciciosDoWhile/Ejercicio6.java
+++ src/ejerciciosDoWhile/Ejercicio6.java
@@ -7,60 +7,62 @@ public class Ejercicio6 {
 		// creamos constantes
 		final String PIEDRA = "Piedra", PAPEL = "Papel", TIJERA = "Tijera",S="S";
 		// creamos las variables no constantes para cada jugada
-		String jugada1, jugada2, menu="n";
+		String jugada1, jugada2, menu;
 		// creamos el escaner
 		Scanner sc = new Scanner(System.in);
-		// presentamos el juego
-		System.out.println("Bienvenido al juego de Piedra, Papel, Tijeras");
-	
 		
+		//creamos el bucle que permitirá reiniciar el juego cuando este acabe
+	do {
+		// presentamos el juego con el menú dentro del primer bucle
+		System.out.println("Bienvenido al juego de Piedra, Papel, Tijeras");
+	//creamos otro bucle para la primera jugada
 			do {
-			System.out.println("Introduce tu jugada, jugador 1");
-			jugada1 = sc.nextLine();
+				//la pantalla debe ir dentro del bucle junto con la captura del escaner
+				System.out.println("Introduce tu jugada, jugador 1");
+				jugada1 = sc.nextLine();
+			}while (!jugada1.equalsIgnoreCase(PIEDRA) && !jugada1.equalsIgnoreCase(PAPEL)
+				&& !jugada1.equalsIgnoreCase(TIJERA));
+		
+		
 
-			}while (!jugada1.equalsIgnoreCase(PIEDRA) || !jugada1.equalsIgnoreCase(PAPEL)
-				|| !jugada1.equalsIgnoreCase(TIJERA));
-		System.out.println("El valor introducido no es válido.");
-		System.out.println("Introduce tu jugada, jugador 1");
-		jugada1 = sc.nextLine();
-
+				//creamos el siguiente bucle de la segunda jugada igual que la primera
+			do {
 			
-				
-			do {
-			System.out.println("Introduce tu jugada, jugador 2");
-			jugada2 = sc.nextLine();
+				System.out.println("Introduce tu jugada, jugador 2");
+				jugada2 = sc.nextLine();
 
-			}while (!jugada2.equalsIgnoreCase(PIEDRA) || !jugada2.equalsIgnoreCase(PAPEL)
-				|| !jugada2.equalsIgnoreCase(TIJERA));
-		System.out.println("El valor introducido no es válido.");
+			}while (!jugada2.equalsIgnoreCase(PIEDRA) && !jugada2.equalsIgnoreCase(PAPEL)
+					&& !jugada2.equalsIgnoreCase(TIJERA));
+			
+
 		
 		System.out.println("Jugador 1 jugó "+jugada1+", Jugador 2 jugó "+jugada2);
-		// A contrinuación creamos las condiciones para cada combinación de jugadas
+		// A contrinuación creamos las condiciones para cada combinación de jugadas 
 		if (jugada1.equals(PIEDRA)&& jugada2.equals(PAPEL)){
 			//Mostramos el resultado de la jugada
 			System.out.println("¡El jugador 2 gana!");
-		}else if(jugada1.equals(PIEDRA)&&jugada2.equals(TIJERA)) {
+		}else if(jugada1.equalsIgnoreCase(PIEDRA)&&jugada2.equalsIgnoreCase(TIJERA)) {
 			System.out.println("¡El Jugador 1 gana!");
-		}else if(jugada1.equals(PIEDRA)&&jugada2.equals(PIEDRA)) {
+		}else if(jugada1.equalsIgnoreCase(PIEDRA)&&jugada2.equalsIgnoreCase(PIEDRA)) {
 			System.out.println("¡Empate!");
-		}else if(jugada1.equals(PAPEL)&&jugada2.equals(PIEDRA)) {
+		}else if(jugada1.equalsIgnoreCase(PAPEL)&&jugada2.equalsIgnoreCase(PIEDRA)) {
 			System.out.println("¡El Jugador 1 gana!");
-		}else if(jugada1.equals(PAPEL)&&jugada2.equals(PAPEL)) {
+		}else if(jugada1.equalsIgnoreCase(PAPEL)&&jugada2.equalsIgnoreCase(PAPEL)) {
 			System.out.println("¡Empate!");
-		}else if(jugada1.equals(PAPEL)&&jugada2.equals(TIJERA)) {
+		}else if(jugada1.equalsIgnoreCase(PAPEL)&&jugada2.equalsIgnoreCase(TIJERA)) {
 			System.out.println("¡El Jugador 2 gana!");
 		}else if(jugada1.equals(TIJERA)&&jugada2.equals(PIEDRA)) {
 			System.out.println("¡El Jugador 2 gana!");
-		}else if(jugada1.equals(TIJERA)&&jugada2.equals(TIJERA)) {
+		}else if(jugada1.equalsIgnoreCase(TIJERA)&&jugada2.equalsIgnoreCase(TIJERA)) {
 			System.out.println("¡Empate!");
-		}else if(jugada1.equals(TIJERA)&&jugada2.equals(PAPEL)) { 
+		}else if(jugada1.equalsIgnoreCase(TIJERA)&&jugada2.equalsIgnoreCase(PAPEL)) { 
 			System.out.println("¡El Jugador 2 gana!");
 		}
 			System.out.println("¿Quieres volver a jugar? Si(Introduce S)/No");
 		menu=sc.nextLine();
 		
 		
-		while(menu.equalsIgnoreCase(S));
+	}while(menu.equalsIgnoreCase(S));
 		
 		sc.close();
 		
