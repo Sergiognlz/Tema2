3c1754abc271738f2fd35a103c3fa99dd5cd7f8f
Sergiognlz <sergio.gonzalez@iesnervion.es>
2023-10-31 12:37:21 +0100

    Merge branch 'master' of https://github.com/Sergiognlz/Tema2.git

---
+++ src/ejerciciosFor/Ejercicio10.java
@@ -0,0 +1,9 @@
+package ejerciciosFor;
+ 
+//Realiza un programa que pida dos números enteros A y B, siendo B mayor que A. Luego visualiza los números desde A hasta B.
+
+public class Ejercicio10 {
+public static void main(String[] args) {
+	
+}
+}
--- src/ejerciciosFor/Ejercicio9.java
+++ src/ejerciciosFor/Ejercicio9.java
@@ -7,30 +7,32 @@ public class Ejercicio9 {
 	public static void main(String[] args) {
 		// creamos las variables
 		int numero;
-		boolean primo=true, noprimo=false;
+		boolean primo = true;
 		// creamos escaner
 		Scanner sc = new Scanner(System.in);
 		// pedimos un número al usuario
 		System.out.println("Introduce un número");
 		// guardamos el número introducido
 		numero = sc.nextInt();
-		if (numero != 1) {
-			for (int i = 2; i <= numero / 2; i++) {
+		if (numero > 1) {
+			for (int i = 2; i < numero ; i++) {
 				if (numero % i == 0) {
-					primo = true;
-				} else {
 					primo = false;
+					break;
 				}
 			}
-			if (primo==false) {
-				System.out.println("No es primo");
-			}
-
-			else {
-				System.out.println("Es primo");
-
-			}
-			sc.close();
+		}else if(numero==1) {
+			primo=false;
 		}
+		if (primo == false) {
+			System.out.println("No es primo");
+		}
+
+		else {
+			System.out.println("Es primo");
+
+		}
+		sc.close();
+
 	}
 }
