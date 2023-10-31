6a9e9d6d33be6c8a48badbf896c9e532e756f675
Sergiognlz <sergio.gonzalez@iesnervion.es>
2023-10-31 12:17:18 +0100

    9

--- src/ejerciciosFor/Ejercicio9.java
+++ src/ejerciciosFor/Ejercicio9.java
@@ -4,31 +4,33 @@ import java.util.Scanner;
 
 //Realiza un programa en java que pida un número entero positivo y nos diga si es primo o no.
 public class Ejercicio9 {
-public static void main(String[] args) {
-	//creamos las variables
-	int numero;
-	boolean primo,noprimo;
-	//creamos escaner
-	Scanner sc=new Scanner(System.in);
-	//pedimos un número al usuario
-	System.out.println("Introduce un número");
-	//guardamos el número introducido
-	numero=sc.nextInt();
-	if(numero!=1) {
-	for(int i=2; i<=numero/2;i++)	{
-	if (numero%i==0) {
-		primo=true;
-	
-	}
-		}
-	}else {
-		primo=false;
-	}
-		if(true) {
-	System.out.println("No es primo");}
-	
-else {
-		System.out.println("Es primo");
+	public static void main(String[] args) {
+		// creamos las variables
+		int numero;
+		boolean primo=true, noprimo=false;
+		// creamos escaner
+		Scanner sc = new Scanner(System.in);
+		// pedimos un número al usuario
+		System.out.println("Introduce un número");
+		// guardamos el número introducido
+		numero = sc.nextInt();
+		if (numero != 1) {
+			for (int i = 2; i <= numero / 2; i++) {
+				if (numero % i == 0) {
+					primo = true;
+				} else {
+					primo = false;
+				}
+			}
+			if (primo==false) {
+				System.out.println("No es primo");
+			}
 
+			else {
+				System.out.println("Es primo");
+
+			}
+			sc.close();
+		}
+	}
 }
-}}
