5519320995ca86fffac3b660aee231b6f3e3df3c
sgonzalez <sgonzalez@106-03.info2>
2024-10-04 13:37:54 +0200

    act

---
+++ Tema2/src/BoletinIfElse/Ejercicio2.java
@@ -0,0 +1,12 @@
+package BoletinIfElse;
+
+public class Ejercicio2 {
+	
+	/*Pedir dos números enteros y decir si son iguales o no.*/
+	
+	public static void main(String[] args) {
+		
+		//creamos variable num
+	}
+
+}
