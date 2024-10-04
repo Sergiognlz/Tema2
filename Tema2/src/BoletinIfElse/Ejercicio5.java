cd1b684795c3652525c1595df400ff33c72e2fe0
sgonzalez <sgonzalez@106-03.info2>
2024-10-04 14:29:57 +0200

    hasta 5

---
+++ Tema2/src/BoletinIfElse/Ejercicio5.java
@@ -0,0 +1,68 @@
+package BoletinIfElse;
+
+import java.nio.channels.AsynchronousSocketChannel;
+import java.util.Scanner;
+
+public class Ejercicio5 {
+/*Pedir tres números y mostrarlos ordenados de mayor a menor.*/
+	
+	public static void main(String[] args) {
+		//creamos variables para los 3 números
+		int num1;
+		int num2;
+		int num3;
+		//creamos escaner
+		Scanner sc=new Scanner(System.in);
+		
+		//pedimos los 3 números al usuario
+		System.out.println("Introduce 3 números");
+		//leemos los 3 números
+		num1=sc.nextInt();
+		num2=sc.nextInt();
+		num3=sc.nextInt();
+		
+		//creamos un if else para comprobar qué número es mayor y ordenarlos
+		if(num1>num2&&num2>num3) {
+			//imprimimos por pantalla orden
+			System.out.println(num1+">"+num2+">"+num3);
+			
+		}else if(num1<num3&&num3>num2){
+			//imprimimos por pantalla orden
+			System.out.println(num1+">"+num3+">"+num2);
+			
+		}else if(num1==num2&&num2>num3) {
+			//imprimimos por pantalla orden
+			System.out.println(num1+"="+num2+">"+num3);
+		}else if(num1>num2&&num2==num3) {
+			//imprimimos por pantalla orden
+			System.out.println(num1+">"+num2+"="+num3);
+		}else if(num2>num1&&num1>num3){
+			//imprimimos por pantalla orden
+			System.out.println(num2+">"+num1+">"+num3);
+		}else if(num2>num3&&num3>num1){
+			//imprimimos por pantalla orden
+			System.out.println(num2+">"+num3+">"+num1);
+		}else if(num2==num1&&num2>num3){
+			//imprimimos por pantalla orden
+			System.out.println(num2+"="+num1+">"+num3);
+		}else if(num2>num1&&num1==num3){
+			//imprimimos por pantalla orden
+			System.out.println(num2+">"+num1+"="+num3);
+		}else if(num3>num1&&num1>num2){
+			//imprimimos por pantalla orden
+			System.out.println(num3+">"+num1+">"+num2);
+		}else if(num3>num2&&num2>num1){
+			//imprimimos por pantalla orden
+			System.out.println(num3+">"+num2+">"+num1);
+		}else if(num3==num1&&num1>num2){
+			//imprimimos por pantalla orden
+			System.out.println(num3+"="+num1+">"+num2);
+		}else if(num3>num1&&num1==num2){
+			//imprimimos por pantalla orden
+			System.out.println(num3+">"+num1+"="+num2);
+		}else {
+			System.out.println(num1+"="+num2+"="+num3);
+		}
+		sc.close();
+	}
+}
