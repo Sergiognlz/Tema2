00684164b944b04818a9b5169b520d47e1a721c9
sgonzalez <sgonzalez@106-03.info2>
2024-10-04 13:33:23 +0200

    1 terminado

--- Tema2/src/BoletinIfElse/Ejercicio1.java
+++ Tema2/src/BoletinIfElse/Ejercicio1.java
@@ -1,5 +1,43 @@
 package BoletinIfElse;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
+	/*Diseñar una aplicación que solicite al usuario un número e indique si es par o impar.*/
+	
+	public static void main(String[] args) {
+		
+		//creamos variable para guardar un número
+		int num;
+		//creamos escaner
+		Scanner sc=new Scanner(System.in);
+		
+		//pedimos un número al usuario
+		System.out.println("Introduce un número");
+		//leemos el número
+		num=sc.nextInt();
+		
+		//creamos un if else para comprobar si es par o impar
+		
+		//comprueba si el número introducido es 0
+		if(num==0) {
+			//mensaje de 0
+			
+			System.out.println("El número introducido es 0");
+			//comprueba que el número introducido es par
+		}else if(num%2==0) {
+			//mensaje de número par
+		
+			System.out.println("El número introducido es par");
+			//en caso contrario es impar
+		}else {
+			//mensaje de número impar
+			
+			System.out.println("El número es impar");
+		}
+		//cerramos escaner
+		sc.close();
+	}
+	
 }
