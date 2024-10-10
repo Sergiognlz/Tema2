8291fa13aff6fd605c4c62fd780649afd15a98ac
Sergio <Sergio@192.168.1.132>
2024-10-10 19:08:44 +0200

    1 condicionales

---
+++ Tema2/src/condicionales/Ejercicio1.java
@@ -0,0 +1,29 @@
+package condicionales;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+	/*
+	 * Escribe una aplicación que solicite al usuario un número comprendido entre 0
+	 * y 9999. La aplicación tendrá que indicar si el número introducido es capicúa.
+	 * Un número es capicúa si se lee igual de izquierda a derecha que de derecha a
+	 * izquierda.
+	 */
+	
+	/*rollo pruebas*/
+	
+	public static void main(String[] args) {
+		
+		//vamos a crear variables para unidades, decenas, centenas y millares
+		int unidades;
+		int decenas;
+		int centenas;
+		int millares;
+		//una variable para guardar el número
+		int num;
+		//creamos escaner
+		Scanner sc=new Scanner(System.in);
+		
+	}
+
+}
