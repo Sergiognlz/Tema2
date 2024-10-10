613ae09e8ffe7e145b4b86c3788baa36a9814393
Sergio <Sergio@192.168.1.132>
2024-10-10 21:38:18 +0200

    4 acabado mothefucker

--- Tema2/src/condicionales/Ejercicio4.java
+++ Tema2/src/condicionales/Ejercicio4.java
@@ -12,26 +12,14 @@ public class Ejercicio4 {
 	
 	//Putas pruebas
 	 /* Entrada 		Esperado 		Resultado
-	 * 0				0				0
-	 * -15				15				15
-	 * 30				30				30
+	 * 		1				Uno				Uno
+	 * 		33			Treinta y tres		Treinta y tres
+	 *		100			fuera rango			fuera rango
 	 */
 	public static void main(String[] args) {
 		//creamos una variable donde guardar el número que introduciremos
 		int num;
-		//creamos cadenas de los números para agilizar
-		final String UNO="uno";
-		final String DOS="dos";
-		final String TRES="tres";
-		final String CUATRO="cuatro";
-		final String CINCO="cinco";
-		final String SEIS="seis";
-		final String SIETE="siete";
-		final String OCHO="ocho";
-		final String NUEVE="nueve";
-		final String DIECI="dieci";
-		final String VEINTI="veinti";
-		
+
 		//creamos escaner
 		Scanner sc=new Scanner(System.in);
 		
@@ -41,31 +29,108 @@ public class Ejercicio4 {
 		num=sc.nextInt();
 		//comprobamos mediante un if else que el número se encuentre entre el rango deseado
 		if(num>=1&&num<=99) {
-			//dividiremos los números menores de 20 que necesitan cadenas individuales ya que no se repiten
-			if(num<=20) {
-			//vamos a crear un switch para los números que vayan del 1 al 20
+		
+	
 			switch(num) {
-			case 1->System.out.println(UNO);
-			case 2->System.out.println(DOS);
-			case 3->System.out.println(TRES);
-			case 4->System.out.println(CUATRO);
-			case 5->System.out.println(CINCO);
-			case 6->System.out.println(SEIS);
-			case 7->System.out.println(SIETE);
-			case 8->System.out.println(OCHO);
-			case 9->System.out.println(NUEVE);
-			case 10->System.out.println("diez");
-			case 11->System.out.println("once");
-			case 12->System.out.println("doce");
-			case 13->System.out.println("trece");
-			case 14->System.out.println("catorce");
-			case 15->System.out.println("quince");
-			case 16->System.out.println("dieciséis");
-			case 17->System.out.println(DIECI+SIETE);
-			case 18->System.out.println(DIECI+OCHO);
-			case 19->System.out.println(DIECI+NUEVE);
-			case 20->System.out.println("veinte");
-			}
+			case 1->System.out.println("Uno");
+			case 2->System.out.println("Dos");
+			case 3->System.out.println("Tres");
+			case 4->System.out.println("Cuatro");
+			case 5->System.out.println("Cinco");
+			case 6->System.out.println("Seis");
+			case 7->System.out.println("Siete");
+			case 8->System.out.println("Ocho");
+			case 9->System.out.println("Nueve");
+			case 10->System.out.println("Diez");
+			case 11->System.out.println("Once");
+			case 12->System.out.println("Doce");
+			case 13->System.out.println("Trece");
+			case 14->System.out.println("Catorce");
+			case 15->System.out.println("Quince");
+			case 16->System.out.println("Dieciséis");
+			case 17->System.out.println("Diecisiete");
+			case 18->System.out.println("Dieciocho");
+			case 19->System.out.println("Diecinueve");
+			case 20->System.out.println("Veinte");
+			case 21->System.out.println("Veintiuno");
+			case 22->System.out.println("Veintidos");
+			case 23->System.out.println("Veintitres");
+			case 24->System.out.println("Veinticuatro");
+			case 25->System.out.println("Veinticinco");
+			case 26->System.out.println("Veintiséis");
+			case 27->System.out.println("Veintisiete");
+			case 28->System.out.println("Veintiocho");
+			case 29->System.out.println("Veintinueve");
+			case 30->System.out.println("Treinta");
+			case 31->System.out.println("Treinta y uno");
+			case 32->System.out.println("Treinta y dos");
+			case 33->System.out.println("Treinta y tres");
+			case 34->System.out.println("Treinta y cuatro");
+			case 35->System.out.println("Treinta y cinco");
+			case 36->System.out.println("Treinta y seis");
+			case 37->System.out.println("Treinta y siete");
+			case 38->System.out.println("Treinta y ocho");
+			case 39->System.out.println("Treinta y nueve");
+			case 40->System.out.println("Cuarenta");
+			case 41->System.out.println("Cuarenta y uno");
+			case 42->System.out.println("Cuarenta y dos");
+			case 43->System.out.println("Cuarenta y tres");
+			case 44->System.out.println("Cuarenta y cuatro");
+			case 45->System.out.println("Cuarenta y cinco");
+			case 46->System.out.println("Cuarenta y seis");
+			case 47->System.out.println("Cuarenta y siete");
+			case 48->System.out.println("Cuarenta y ocho");
+			case 49->System.out.println("Cuarenta y nueve");
+			case 50->System.out.println("Cincuenta");
+			case 51->System.out.println("Cincuenta y uno");
+			case 52->System.out.println("Cincuenta y dos");
+			case 53->System.out.println("Cincuenta y tres");
+			case 54->System.out.println("Cincuenta y cuatro");
+			case 55->System.out.println("Cincuenta y cinco");
+			case 56->System.out.println("Cincuenta y seis");
+			case 57->System.out.println("Cincuenta y siete");
+			case 58->System.out.println("Cincuenta y ocho");
+			case 59->System.out.println("Cincuenta y nueve");
+			case 60->System.out.println("Sesenta");
+			case 61->System.out.println("Sesenta y uno");
+			case 62->System.out.println("Sesenta y dos");
+			case 63->System.out.println("Sesenta y tres");
+			case 64->System.out.println("Sesenta y cuatro");
+			case 65->System.out.println("Sesenta y cinco");
+			case 66->System.out.println("Sesenta y seis");
+			case 67->System.out.println("Sesenta y siete");
+			case 68->System.out.println("Sesenta y ocho");
+			case 69->System.out.println("Sesenta y nueve");
+			case 70->System.out.println("Setenta");
+			case 71->System.out.println("Setenta y uno");
+			case 72->System.out.println("Setenta y dos");
+			case 73->System.out.println("Setenta y tres");
+			case 74->System.out.println("Setenta y cuatro");
+			case 75->System.out.println("Setenta y cinco");
+			case 76->System.out.println("Setenta y seis");
+			case 77->System.out.println("Setenta y siete");
+			case 78->System.out.println("Setenta y ocho");
+			case 79->System.out.println("Setenta y nueve");
+			case 80->System.out.println("Ochenta");
+			case 81->System.out.println("Ochenta y uno");
+			case 82->System.out.println("Ochenta y dos");
+			case 83->System.out.println("Ochenta y tres");
+			case 84->System.out.println("Ochenta y cuatro");
+			case 85->System.out.println("Ochenta y cinco");
+			case 86->System.out.println("Ochenta y seis");
+			case 87->System.out.println("Ochenta y siete");
+			case 88->System.out.println("Ochenta y ocho");
+			case 89->System.out.println("Ochenta y nueve");
+			case 90->System.out.println("Noventa");
+			case 91->System.out.println("Noventa y uno");
+			case 92->System.out.println("Noventa y dos");
+			case 93->System.out.println("Noventa y tres");
+			case 94->System.out.println("Noventa y cuatro");
+			case 95->System.out.println("Noventa y cinco");
+			case 96->System.out.println("Noventa y seis");
+			case 97->System.out.println("Noventa y siete");
+			case 98->System.out.println("Noventa y ocho");
+			case 99->System.out.println("Noventa y nueve");
 			
 			
 			}
