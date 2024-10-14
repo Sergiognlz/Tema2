09f9d0e13b2efbaa32cb4ea23fe1231af0815bca
sgonzalez <sgonzalez@106-03.info2>
2024-10-14 11:55:08 +0200

    6

--- Tema2/src/condicionales/Ejercicio5.java
+++ Tema2/src/condicionales/Ejercicio5.java
@@ -1,5 +1,6 @@
 package condicionales;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Ejercicio5 {
@@ -11,21 +12,26 @@ public class Ejercicio5 {
 	 */
 
 	public static void main(String[] args) {
-
+		//clase random
+		Random rand=new Random();
 		// creamos dos variables para almacenar los números aleatorios mediante un
 		// math.random. Es necesario castear porque genera double y sumar uno para eviar
 		// el 0
-		int num1 = (int) (Math.random() * 99) + 1;
-		int num2 = (int) (Math.random() * 99) + 1;
+		int num1 = rand.nextInt(1,100);
+		int num2 = rand.nextInt(1,100);
 		// creamos una variable donde almacenar el número que introducirá el usuario
 		int respuesta;
 		// creamos escaner
 		Scanner sc = new Scanner(System.in);
 		
 		//pedimos al usuario que adivine la suma
-		System.out.println("Adivina la suma de dos números al azar");
+		System.out.println("Adivina la suma de dos números al azar entre 1 y 99");
 		//guardamos el número el usuario
 		respuesta=sc.nextInt();
+		//controlamos el rango
+		if((num1<0&&num1>198)&& num2<0&&num1>198) {
+			System.out.println("El número introducido está fuera de rango");
+		}
 		
 		//si la respuesta coincide con la suma habrá acertado el juego
 		if((num1+num2)==respuesta) {
@@ -40,6 +46,7 @@ public class Ejercicio5 {
 			System.out.println("Número 2: "+num2);
 			System.out.println("No has acerdado la suma de los números");
 		}
+	
 		sc.close();
 	}
 }
