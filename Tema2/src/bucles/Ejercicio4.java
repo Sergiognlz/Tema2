d46f53d91fb99957a75d3bdb260e64920545927b
Sergio <Sergio@192.168.1.134>
2024-10-28 19:14:20 +0100

    4

--- Tema2/src/bucles/Ejercicio4.java
+++ Tema2/src/bucles/Ejercicio4.java
@@ -1,5 +1,7 @@
 package bucles;
 
+import java.util.Scanner;
+
 public class Ejercicio4 {
 	/*
 	 * De forma similar a la actividad 4, realiza un programa que calcule el mínimo
@@ -7,6 +9,53 @@ public class Ejercicio4 {
 	 * máximo de los dos e ir incrementando hasta encontrar el primer número que sea
 	 * múltiplo de los dos números.
 	 */
-	
-	
+	public static void main(String[] args) {
+
+		// creamos variables para a y b
+		int a;
+		int b;
+		// variable donde guardar el mayor y menor
+		int mayor = 0;
+		int menor = 0;
+		// varable donde guardar el divisor
+		int multi = 1;
+
+		// escaner
+		Scanner sc = new Scanner(System.in);
+
+		// control de rango
+		do {
+			// pedimos número A
+			System.out.println("Introduce número A");
+			// guardamos
+			a = sc.nextInt();
+		} while (a < 0);
+		do {
+			// pedimos número B
+			System.out.println("Introduce número B");
+			// guardamos
+			b = sc.nextInt();
+		} while (b < 0);
+
+		if (a >= b) {
+			mayor = a;
+			menor = b;
+		} else if (a < b) {
+			mayor = b;
+			menor = a;
+
+		}
+
+		for (int i = mayor; i > 0; i++) {
+			if ((i % mayor == 0) && (i % menor == 0)) {
+				multi = i;
+				break;
+			}
+
+		}
+		System.out.println("El máximo común múltiplo es " + multi);
+
+		// cerramos escaner
+		sc.close();
+	}
 }
