77020c2323f1d9a26e3d6591e9379cb4641f27f4
sgonzalez <sgonzalez@106-03.info2>
2024-10-28 11:48:33 +0100

    dff

--- Tema2/src/bucles/Ejercicio2.java
+++ Tema2/src/bucles/Ejercicio2.java
@@ -11,7 +11,8 @@ public class Ejercicio2 {
 		// variable número
 		int num;
 		// contador
-		int primos = 0;
+		int primos =1;
+		boolean primo=false;
 		// escaner
 		Scanner sc = new Scanner(System.in);
 
@@ -21,15 +22,27 @@ public class Ejercicio2 {
 			System.out.println("Introduce un número");
 			// guardamos el valor
 			num = sc.nextInt();
-		} while (num < 1);
-		for (int i = 2; i <= num; i++) {
-			for (int j = 1; j < i; j++) {
+		} while (num <= 2);
+		for (int i = 3; i <= num; i++) {
+			
+			
+			
+			
+			for (int j = 1; j < i; ) {
+				j++;
 				// if cuando el número sea divisible por el número
-				if (i % j = 0) {
-					primos++;
-					
+				if (i % j == 0) {
+					primo=false;
+				
+				}else {
+				primo=true;	
 				}
-
+					
+			
+				
+			}
+			if(primo) {
+				primos++;
 			}
 		}
 		// imprimimos resultado
