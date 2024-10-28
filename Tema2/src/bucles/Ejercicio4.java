07f3b94391d2ebbf1b1e818e77b9c71f913b9e1f
sgonzalez <sgonzalez@106-03.info2>
2024-10-28 12:42:29 +0100

    4 iniciado

---
+++ Tema2/src/bucles/Ejercicio4.java
@@ -0,0 +1,12 @@
+package bucles;
+
+public class Ejercicio4 {
+	/*
+	 * De forma similar a la actividad 4, realiza un programa que calcule el mínimo
+	 * común múltiplo de dos números dados. En este caso, habrá que partir del
+	 * máximo de los dos e ir incrementando hasta encontrar el primer número que sea
+	 * múltiplo de los dos números.
+	 */
+	
+	
+}
