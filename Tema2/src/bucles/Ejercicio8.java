bdeaeb51172bfc17238c04b66c5bbb413ceebe62
Sergio <Sergio@192.168.1.134>
2024-10-29 20:10:28 +0100

    act

--- Tema2/src/bucles/Ejercicio8.java
+++ Tema2/src/bucles/Ejercicio8.java
@@ -27,17 +27,24 @@ public class Ejercicio8 {
 			System.out.println("Introduce un número");
 			// guardamos
 			num = sc.nextInt();
+			//un if para actuar de distinta manera dependiendo del valor introducido
 			if (num >= mayor) {
+				//si el número introducido es mayor o igual que el mayor introducido hasta el momento se asigna como nuevo valor mayor
 				mayor = num;
+				//se incrementa el contador
+				cont++;
+			//si el número es 0 no se incrementa contador ni fallados, y sale del bucle
 			} else if (num == 0) {
-
+				//en cualquier otro caso (que número introducido sea menor que el valor mayor) se cuenta el número, y además se cuenta como fallado
 			} else {
+				cont++;
 				fallados++;
 			}
 		} while (num != 0);
-
+		//imprimimos resultados
 		System.out.println("A. Total de números introducidos: " + cont);
 		System.out.println("B. Números fallados " + fallados);
+		//cerramos escaner
 		sc.close();
 	}
 }
---
+++ Tema2/src/bucles/Ejercicio9.java
@@ -0,0 +1,11 @@
+package bucles;
+
+public class Ejercicio9 {
+	/*
+	 * Realiza un programa que nos diga cuántos dígitos tiene un número introducido
+	 * por teclado. El número introducido debe ser mayor que 0.
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
